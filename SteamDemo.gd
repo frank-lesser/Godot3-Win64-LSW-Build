5f3d88772329a6956db54dea83d54448ff34b84c
Frank Lesser <frank-lesser@lesser-software.com>
2018-03-20 22:08:39 +0100

    Delete SteamDemo.gd

--- SteamDemo.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-# Steam library
-var Steam_module = preload("res://bin/Steam.gdns");
-onready var stream = Steam.new();								# create a SQLite instance
-
-func _ready():
-
-# add code ass described at https://gramps.github.io/GodotSteam
